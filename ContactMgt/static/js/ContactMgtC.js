542da5bb022cece5245488fed630e5ed09882bf6
Afshan <afshan.ramzan@zaxiss.com>
2020-10-11 23:27:46 -0700

    Ui for contact management system

---
+++ ContactMgt/static/js/ContactMgtC.js
@@ -0,0 +1,11 @@
+/* globals Shared */
+
+(function() {
+
+  ContactMgt.ContactMgtC = Trillo.inherits(Shared.AppC, function(viewSpec) {
+    Shared.AppC.call(this, viewSpec);
+  });
+
+  var ContactMgtC = ContactMgt.ContactMgtC.prototype;
+  var AppC = Shared.AppC.prototype;
+})();
